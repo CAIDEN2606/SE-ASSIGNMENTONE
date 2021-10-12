df7ee547eda375883f0c120db55b179a243e5a98
Caiden Farmer <409854@edu.cwa.ac.uk>
2021-10-12 19:33:43 +0100

    centered test lbl - test change

---
+++ MUSK_Inspections/GeneralInspection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MUSK_Inspections
+{
+    public partial class GeneralInspection : Form
+    {
+        public GeneralInspection()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_General_Inpection_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

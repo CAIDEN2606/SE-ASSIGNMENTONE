9e99d056c4d1773958f7192ff5639fb77e7a14bf
Caiden Farmer <409854@edu.cwa.ac.uk>
2021-10-13 16:10:33 +0100

    test

--- MUSK_Inspections/Form1.Designer.cs
+++ MUSK_Inspections/Form1.Designer.cs
@@ -35,20 +35,20 @@
             // 
             this.lbl_Test.AutoSize = true;
             this.lbl_Test.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_Test.Location = new System.Drawing.Point(754, 9);
-            this.lbl_Test.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.lbl_Test.Location = new System.Drawing.Point(546, 50);
+            this.lbl_Test.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbl_Test.Name = "lbl_Test";
-            this.lbl_Test.Size = new System.Drawing.Size(102, 48);
+            this.lbl_Test.Size = new System.Drawing.Size(68, 31);
             this.lbl_Test.TabIndex = 0;
             this.lbl_Test.Text = "Test";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1600, 865);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.lbl_Test);
-            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
